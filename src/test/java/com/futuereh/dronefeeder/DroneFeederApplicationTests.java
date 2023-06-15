8c747c4d34d90b1126b22fedc22982d32b9a1bbb
stonefullstm <stonefull.stm@gmail.com>
2023-06-15 20:14:34 -0300

    test: Criar teste da rota GET /drones - quando houver drones no banco de dados

--- src/test/java/com/futuereh/dronefeeder/DroneFeederApplicationTests.java
+++ src/test/java/com/futuereh/dronefeeder/DroneFeederApplicationTests.java
@@ -1,10 +1,14 @@
 package com.futuereh.dronefeeder;
 
 import com.futuereh.dronefeeder.dto.DroneDto;
+import com.futuereh.dronefeeder.model.Drone;
+import com.futuereh.dronefeeder.repository.DroneRepository;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -13,9 +17,9 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -24,6 +28,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 class DroneFeederApplicationTests {
   @Autowired
   private MockMvc mockMvc;
+
+  @SpyBean
+  DroneRepository droneRepository;
+
+  @BeforeEach
+  public void setup() {
+    droneRepository.deleteAll();
+  }
 
   @Test
   @Order(1)
@@ -38,5 +50,22 @@ class DroneFeederApplicationTests {
             .content(new ObjectMapper().writeValueAsString(droneDto)))
         .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
         .andExpect(jsonPath("$.nome").value(droneDto.getNome()));
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("2 - Deve retornar todas os drones existentes da base de dados.")
+  void deveRetornarTodosDronesExistentesNaBase() throws Exception {
+    Drone drone1 = new Drone();
+    drone1.setNome("Drone 1");
+    Drone drone2 = new Drone();
+    drone2.setNome("Drone 2");
+    droneRepository.save(drone1);
+    droneRepository.save(drone2);
+
+    mockMvc.perform(get("/drones").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].nome").value(drone1.getNome()))
+        .andExpect(jsonPath("$[1].nome").value(drone2.getNome()));
   }
 }
